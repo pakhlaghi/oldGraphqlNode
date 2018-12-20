3cff934b510eb7b76574ad92e66c45cb4ca92dd1
Peyman Akhlaghi <codecore@Peymans-MBP.gateway>
2018-12-20 22:17:13 +1100

    merge

--- client/src/app/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Home from './Home/homeContainer';
-
-const App = () => <Home />;
-
-export default App;
--- client/src/app/home/service/operations.js
+++
@@ -1,8 +0,0 @@
-import actions from './actions';
-
-const { incrementCount, decrementCount } = actions;
-
-export default {
-  incrementCount,
-  decrementCount,
-};
