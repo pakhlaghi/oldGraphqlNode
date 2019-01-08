7c5d0eb4fb07f960b8e58d3a1a666316015065db
Peyman Akhlaghi <codecore@Peymans-MBP.gateway>
2019-01-08 23:52:42 +1100

    modified webpack

--- client/webpack/webpack.dev.js
+++ client/webpack/webpack.dev.js
@@ -2,6 +2,8 @@ const webpack = require("webpack");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   devtool: "inline-source-map",
@@ -13,6 +15,7 @@ module.exports = merge(common, {
   plugins: [
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("development")
-    })
+    }),
+  new BundleAnalyzerPlugin()
   ]
 });
